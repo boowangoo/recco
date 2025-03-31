b14a34e33402893a45695307f9a814b220c4ca3a
boowangoo <bfwang@utexas.edu>
2025-03-30 22:33:00 -0400

    removed commented-out code

--- recco-server.Dockerfile
+++ recco-server.Dockerfile
@@ -1,5 +1,4 @@
 FROM golang:1.24.1-bullseye
 RUN apt update && apt install -y vim
 WORKDIR /recco-server
-# ENTRYPOINT ["go", "run", "."]
 
--- start.sh
+++ start.sh
@@ -1,16 +1,1 @@
-# IMAGE="recco-server"
-# CONTAINER="recco-server"
-
-# # no image
-# if [ -z $(docker images -q $IMAGE) ]; then
-#     docker build -t $IMAGE .
-# fi
-
-# # no container
-# if [ -z $(docker ps -q -f name=$CONTAINER) ]; then
-#     docker run --name $CONTAINER -v $(pwd)/recco-server:/recco-server --rm -i -t -p 127.0.0.1:8080:8080 -d $IMAGE
-# fi
-
-# # connect to container
-# docker attach $CONTAINER
 docker-compose up

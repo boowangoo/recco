441eebd40d375b187839292764b6b7b551e6c6db
boowangoo <bfwang@utexas.edu>
2025-04-17 22:09:47 -0400

    add recco_server request, rename recco-server to recco_server

--- recco-server/recco_server.go
+++ recco-server/recco_server.go
@@ -5,6 +5,7 @@ import (
     "net/http"
     "log"
     "io"
+    "encoding/json"
 )
 
 func main() {
@@ -15,6 +16,7 @@ func main() {
 
     http.HandleFunc("/", HelloServer)
     http.HandleFunc("/google-query", GoogleQueryHandler)
+    http.HandleFunc("/qdrant-collection-exists", QdrantCollectionExitsHandler)
     listen_err := http.ListenAndServe(":8080", nil)
     if listen_err != nil {
         fmt.Println("listen err", listen_err)
@@ -53,3 +55,38 @@ func GoogleQueryHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, string(bodyBytes))
 }
 
+func QdrantCollectionExitsHandler(w http.ResponseWriter, r *http.Request) {
+    collection := r.URL.Query().Get("collection")
+    url := fmt.Sprintf("http://recco-db:6333/collections/%s/exists", collection)
+    resp, err := http.Get(url)
+    if err != nil {
+        log.Println(err)
+        fmt.Fprintf(w, "Request Failed")
+        return
+    }
+    if resp.StatusCode != 200 {
+        log.Println(resp.Status)
+        fmt.Fprintf(w, "Request Failed")
+        return
+    }
+
+    type exists_resp struct {
+        Result struct {
+            Exists bool
+        }
+    }
+    var result exists_resp
+    dec := json.NewDecoder(resp.Body)
+    err = dec.Decode(&result)
+    if err != nil{
+        log.Println(err)
+        fmt.Fprintf(w, "Request Failed")
+        return
+    }
+
+    if !result.Result.Exists {
+        fmt.Fprintf(w, "The collection %s does not exist", collection)
+    } else {
+        fmt.Fprintf(w, "The collection %s exist", collection)
+    }
+}

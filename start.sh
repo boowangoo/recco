3efdfee0b19674fa1f40dc36b3bc1a5108e57bcd
boowangoo <bfwang@utexas.edu>
2025-05-11 19:30:53 -0400

    Seperated services into different folders

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
-# TODO: Move recco-db data files to cloud storage
 docker-compose up --build -d
+
+# # Start recco-dataloader to initialize the recco-db
+# # TODO: Move recco-db data files to cloud storage
+# if [[ "$@" == *"-d"* ]]; then
+#     echo "Starting recco-dataloader container"
+#     cd recco-dataloader/ && docker-compose up --build -d
+#     cd ..
+# fi
